ff19faaeae74ae37ad6d5485815ab1132fca5b0f
Md. Ashraful Islam <mdashrafulislam.inbox@gmail.com>
2021-06-28 23:11:02 +0600

    xml_init

---
+++ app/src/main/java/com/mkrlabs/bloodsoilder/ui/account/userinformation/AddProfilePictureActivity.java
@@ -0,0 +1,16 @@
+package com.mkrlabs.bloodsoilder.ui.account.userinformation;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.mkrlabs.bloodsoilder.R;
+
+public class AddProfilePictureActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_profile_picture);
+    }
+}
