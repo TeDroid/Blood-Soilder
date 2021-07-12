892860845b1d8d88ed6cf7212dd65e3fbb2e9db1
Md. Ashraful Islam <mdashrafulislam.inbox@gmail.com>
2021-07-13 02:39:04 +0600

    activity_tab_&_blood_group_selection

--- app/src/main/java/com/mkrlabs/bloodsoilder/ui/activity/ActivityFragment.java
+++ app/src/main/java/com/mkrlabs/bloodsoilder/ui/activity/ActivityFragment.java
@@ -2,59 +2,36 @@ package com.mkrlabs.bloodsoilder.ui.activity;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.widget.AppCompatButton;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.FrameLayout;
 
 import com.mkrlabs.bloodsoilder.R;
+import com.mkrlabs.bloodsoilder.ui.request.donate.DonateFragment;
+import com.mkrlabs.bloodsoilder.ui.request.seeker.SeekerFragment;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link ActivityFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+
 public class ActivityFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    private FrameLayout activityFrameContainer;
+    private AppCompatButton activityCampaignButton, activityHistoryButton;
 
     public ActivityFragment() {
         // Required empty public constructor
+        // new changes applied
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment ActivityFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static ActivityFragment newInstance(String param1, String param2) {
-        ActivityFragment fragment = new ActivityFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+
     }
 
     @Override
@@ -62,5 +39,57 @@ public class ActivityFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_activity, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        init(view);
+        setCurrentButtonStyle(true,  activityCampaignButton, activityHistoryButton,true);
+        setUpWalletFragment(new DonateFragment());
+        activityCampaignButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                setCurrentButtonStyle(true, activityCampaignButton, activityHistoryButton,true);
+                setUpWalletFragment(new DonateFragment());
+            }
+        });
+        activityHistoryButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                setCurrentButtonStyle(true, activityHistoryButton, activityCampaignButton,false);
+                setUpWalletFragment(new SeekerFragment());
+            }
+        });
+
+
+    }
+
+    private void setCurrentButtonStyle(boolean isSelected, AppCompatButton selectedButton, AppCompatButton nonSelectedButton,boolean isDonate) {
+        if (isSelected) {
+            selectedButton.setTextColor(getContext().getResources().getColor(R.color.white));
+
+            if (isDonate){
+                selectedButton.setBackgroundDrawable(getContext().getDrawable(R.drawable.selected_donate_button_style));
+            }else {
+                selectedButton.setBackgroundDrawable(getContext().getDrawable(R.drawable.selected_seek_button_style));
+            }
+
+            // opposite button
+            nonSelectedButton.setTextColor(getContext().getResources().getColor(R.color.primaryColor));
+            nonSelectedButton.setBackgroundDrawable(getContext().getDrawable(R.drawable.non_selected_button_style));
+        }
+    }
+
+    private void setUpWalletFragment(Fragment fragment) {
+        getActivity().getSupportFragmentManager().beginTransaction()
+                .replace(R.id.activityFrameContainer, fragment)
+                .commit();
+    }
+
+    private void init(View view) {
+        activityFrameContainer = view.findViewById(R.id.requestFrameContainer);
+        activityCampaignButton = view.findViewById(R.id.campaignTabButton);
+        activityHistoryButton = view.findViewById(R.id.historyTabButton);
     }
 }
